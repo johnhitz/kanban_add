3d06be11912c16a80e4b10926ce21bedf7be1ddb
John Hitz <johnh@whodapet.com>
2016-04-11 20:04:31 -0500

    Edit notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -30,7 +30,7 @@ export default class App extends React.Component {
     return (
       <div>
         <button onClick={this.addNote}>+</button>
-        <Notes notes={notes} />
+        <Notes notes={notes} onEdit={this.editNote} />
       </div>
     );
   }
@@ -41,5 +41,19 @@ export default class App extends React.Component {
         task: 'New task'
       }])
     });
+  };
+  editNote = (id, task) => {
+    if(!task.trim()) {
+      return;
+    }
+
+    const notes = this.state.notes.map(note => {
+      if(note.id === id && task) {
+        note.task = task;
+      }
+      return note;
+    });
+
+    this.setState({notes});
   };
 }
